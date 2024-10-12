04e05ecb7abd7e2d2b9c4448840081a7869d6ad5
ucha <uchaucha419@gmail.com>
2024-10-12 23:05:55 +0400

    using nltk library tokenizer and stopwords

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import string
 import matplotlib.pyplot as plt
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 
 text = open('text.txt', encoding='utf-8').read()
 
@@ -7,23 +9,11 @@ text = open('text.txt', encoding='utf-8').read()
 text = text.lower().translate(str.maketrans('', '', string.punctuation))
 
 # tokenization
-tokenized_words = text.split()
-
-# stop words are the words that don't add any meaning to text, don't carry any emotion
-stop_words = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself",
-              "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself",
-              "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these",
-              "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do",
-              "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while",
-              "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before",
-              "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again",
-              "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each",
-              "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than",
-              "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
+tokenized_words = word_tokenize(text, 'english')
 
 final_words = []
 for word in tokenized_words:
-    if word not in stop_words:
+    if word not in stopwords.words('english'):
         final_words.append(word)
 
 
